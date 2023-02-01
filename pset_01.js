b4b449a291d666e67d8f05895f17d048057020f4
KhokonPHb07 <kshehala.com>
2023-02-01 07:18:05 +0600

    basic loop problem

--- pset_01.js
+++ pset_01.js
@@ -21,19 +21,40 @@ each year is a leap year. If a year is a leap year insert that year in a
 new array, return the new array and print the result.
  */
 
-function findLeapYear(year) {
+// function findLeapYear(year) {
     
-    for (let i = 0; i < year.length; i++) {
-        
-        if (year[i] % 4 === 0) {
-            console.log(year[i], 'this is leap year')
-        }
-        else {
-            console.log(year[i], 'this is not leap year')
-        }
-    }
-}
-let leapYear = [2023, 2024, 2025, 2028, 2030];
-let knowLeapYear = findLeapYear(leapYear);
-console.log(knowLeapYear);
+//     for (let i = 0; i < year.length; i++) {
+//         let lYear=[];
+//         if (year[i] % 4 === 0) {
+//             console.log(year[i], 'this is leap year');
+//             lYear.push(year[i]);
+//         }
+//         else {
+//             console.log(year[i], 'this is not leap year')
+//         }
+//     }
+//     return lYear;
+// }
+
+
+
+// let leapYear = [2023, 2024, 2025, 2028, 2030];
+// let knowLeapYear = findLeapYear(leapYear);
+// console.log(knowLeapYear);
+// console.log(lYear)
+
+// for(let i=13;i<=13;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(i,'X',j,'=',i*j);
+//     }
+// }
+// pp-01>>>order the number from lower to higher
+let x=[1,2,5,4,3,9,8,7,6];
+//pp-02>>>>delete all duplicate number
+let y=[1,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]
+//pp-03>>>>> find out the largest name
+let z=["rahim","karim","bangladesh","india","china","indonesia","Australia"]
+
+
+
 
